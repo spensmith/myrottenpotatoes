bd570675503b2cb681d7277b0438a14b9d283627
Baochuan Lu <lubaochuan@gmail.com>
2015-10-13 20:59:38 +0000

    Add moviegoer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009204300) do
+ActiveRecord::Schema.define(version: 20151013205201) do
+
+  create_table "moviegoers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
